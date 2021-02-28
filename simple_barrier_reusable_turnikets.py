45137f9831a7829ea5dab834f5202174a271d09d
Kristina Fecova <xfecova@stuba.sk>
2021-02-28 13:55:00 +0100

    simple barrier nabitie turniketu

--- simple_barrier_reusable_turnikets.py
+++ simple_barrier_reusable_turnikets.py
@@ -2,6 +2,7 @@ from fei.ppds import *
 from random import randint
 from time import sleep
 
+
 class SimpleBarrier:
     def __init__(self, N):
         self.N = N
@@ -31,10 +32,20 @@ class SimpleBarrier:
         self.turniket2.wait()
         self.turniket2.signal()
 
+    def barrier_nabitie(self):
+        self.mutex.lock()
+        self.counter = self.counter + 1
+        if self.counter == self.N:
+            self.counter = 0
+            self.turniket_nabitie.signal(self.N)
+        self.mutex.unlock()
+        self.turniket_nabitie.wait()
+
 
 def rendezvous(thread_name):
     sleep(randint(1,10)/10)
     print('rendezvous: %s' % thread_name)
+
 
 def ko(thread_name):
     print('ko: %s' % thread_name)
@@ -47,11 +58,15 @@ nielen pred vykonanim funkcie 'ko', ale aj
 *vzdy* pred zacatim vykonavania funkcie 'rendezvous'.
 """
 
+#instancie pre turnikety s nabitim
+simpleBarrier1 = SimpleBarrier(10)
+simpleBarrier2 = SimpleBarrier(10)
+
+#instancia pre 2 turnikety
 simpleBarrier = SimpleBarrier(10)
 
 
-
-def barrier_example(thread_name,barrier):
+def barrier_example(thread_name, barrier):
 
     while True:
         # ...
@@ -63,9 +78,22 @@ def barrier_example(thread_name,barrier):
         # ...
 
 
+def barrier_example_nabitie(thread_name, barier1, barrier2):
+
+    while True:
+        # ...
+        rendezvous(thread_name)
+        # ...
+        barier1.barrier_nabitie()
+        ko(thread_name)
+        barrier2.barrier_nabitie()
+        # ...
+
+
 threads = list()
 for i in range(10):
-    t = Thread(barrier_example, 'Thread %d' % i,simpleBarrier)
+    # t = Thread(barrier_example, 'Thread %d' % i,simpleBarrier)
+    t = Thread(barrier_example_nabitie, 'Thread %d' % i, simpleBarrier1, simpleBarrier2)
     threads.append(t)
 
 for t in threads:
